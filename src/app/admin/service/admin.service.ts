68ed9f6a1b9fe7a9047fa1570044c8187b85e61b
Alex Ponce <alexponce2753@gmail.com>
2024-09-20 21:54:14 -0500

    agregando componente postCategory y correcion la generacion del token

---
+++ src/app/admin/service/admin.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserStorageService } from 'src/app/services/storage/user-storage.service';
+
+const BASIC_URL = "http://localhost:8080/";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  constructor(private http:HttpClient) { }
+
+  //agregar categorias al dashboard
+
+  addCategory(categoryDto: any):Observable<any>{
+    return this.http.post(BASIC_URL + 'api/admin/category', categoryDto, {
+      headers:this.createAuthorizationHeader()
+    })
+  }
+
+  //observacion tanto como el backend y el frontend el 'Bearer ' -> debe estar con un espacio
+  //el token se generar de forma incorrecta e invalida
+  private createAuthorizationHeader(): HttpHeaders{
+    return new HttpHeaders().set(
+      'Authorization', 'Bearer ' + UserStorageService.getToken()
+    )
+  }
+}
--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerComponent } from './customer.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PostCategoryComponent } from './components/post-category/post-category.component';
 
 
 @NgModule({
   declarations: [
     CustomerComponent,
-    DashboardComponent
+    DashboardComponent,
+    PostCategoryComponent
   ],
   imports: [
     CommonModule,
